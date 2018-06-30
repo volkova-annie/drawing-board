65273d72093f40838c5a5a87dcc811f50f58a74b
Volkova Annie <annie_v@bk.ru>
2018-06-30 18:02:10 +0300

    Fix fill board and add drawing with black color

--- src/components/DrawingBoard/index.js
+++ src/components/DrawingBoard/index.js
@@ -1,35 +1,73 @@
 import React, {Component} from 'react';
-import styles from "./styles.css"
+import styles from './styles.css'
 
 class DrawingPage extends Component {
   constructor(props) {
     super(props);
     this.fill = this.fill.bind(this);
     this.clear = this.clear.bind(this);
+
+    this.state = {
+      mouseDown: false
+    };
   }
 
   componentDidMount() {
     const canvas = this.refs.canvas;
-    this.ctx = canvas.getContext("2d");
+    this.ctx = canvas.getContext('2d');
   }
 
-  fill (color) {
+  fill(color) {
     const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d');
     ctx.fillStyle=color;
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 
-  clear () {
+  clear() {
     const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  onMouseDown() {
+    this.setState({
+      mouseDown: true
+    });
+  }
+
+  onMouseUp() {
+    this.setState({
+      mouseDown: false
+    });
+  }
+
+  onMouseMove(event) {
+    if (this.state.mouseDown) {
+      let position = {x : event.nativeEvent.offsetX, y: event.nativeEvent.offsetY };
+      let size = {width : 20, height: 20 };
+      this.drawRect(position, size);
+    }
+  }
+
+  drawRect(position, size) {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle='black';
+    ctx.fillRect(position.x - size.width / 2, position.y - size.height / 2, size.width, size.height);
   }
 
   render() {
     return (
       <div>
-        <canvas ref="canvas" width={this.props.width} height={this.props.height} className={styles.board}></canvas>
+        <canvas ref='canvas'
+                width={this.props.width}
+                height={this.props.height}
+                className={styles.board}
+                onMouseDown={() => this.onMouseDown()}
+                onMouseUp={() => this.onMouseUp()}
+                onMouseMove={(event) => this.onMouseMove(event)}>
+        </canvas>
       </div>
     )
   }
--- src/components/DrawingPage/index.js
+++ src/components/DrawingPage/index.js
@@ -1,24 +1,29 @@
 import React, {Component} from 'react';
 import DrawingBoard from '../DrawingBoard';
+import classnames from 'classnames';
 import styles from "./styles.css"
 
 class DrawingPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getColor = this.getColor.bind(this);
+  };
+
+  getColor() {
+    const color = document.getElementById('color').value;
+    return color;
+  }
 
   render() {
     return (
       <div className={styles.wrapper}>
         <div className={styles.panel}>
-          <span>Fill:</span>
-          <select defaultValue="default" onChange={(event) => this.board.fill(event.target.value)}>
-            <option disabled value="default">---</option>
-            <option className={styles.blue} value="blue">Blue</option>
-            <option className={styles.red} value="red">Red</option>
-            <option className={styles.green} value="green">Green</option>
-            <option className={styles.gray} value="gray">Gray</option>
-            <option className={styles.yellow} value="yellow">Yellow</option>
-            <option className={styles.black} value="black">Black</option>
-            <option className={styles.white} value="white">White</option>
-          </select>
+          <label htmlFor="color">
+            Choose color:
+          </label>
+          <input id="color" type="color" />
+          <button onClick={() => this.board.fill(this.getColor())}>Fill</button>
           <button onClick={() => this.board.clear()} className={styles.clear}>Clear</button>
         </div>
         <DrawingBoard width="700px" height="500px" ref={instance => {this.board = instance}} />
