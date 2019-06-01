b85d2d9f4ca892498d28df607db7b31afdae59bb
Volkova Annie <annie_v@bk.ru>
2019-06-01 20:37:00 +0300

    Remove minimize option from css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,7 @@ module.exports = {
               modules: true,
               importLoaders: 1,
               localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
+              sourceMap: true
             }
           }
         ]
